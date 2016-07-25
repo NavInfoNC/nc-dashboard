3c2a910ab94f4b54ee97079de08fb4e2e6806bc1
lizj <lizj@mapbar.com>
2016-07-25 10:53:34 +0800

    Refactor.

--- index.py
+++ index.py
@@ -15,31 +15,6 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 server = jenkins.Jenkins('http://build.navicore.mapbar.com', username='robot', password='CheeseSnack')
 
 
-def get_status(info):
-    status = info['color']
-    if status.endswith("_anime"):
-        status = status[0:-6] + " building"
-    return status
-
-
-# return (failed_number, total_number)
-def get_health_report(info):
-    for i in info["healthReport"]:
-        if i["description"].startswith("Test Result"):
-            m = re.match(".*?(\d[,\d]+).*?(\d[,\d]+)", i["description"])
-            if m:
-                failed = m.group(1).replace(",", "")
-                total = m.group(2).replace(",", "")
-                return int(failed), int(total)
-    return 0, 0
-
-
-# return the timestamp of the last build in milliseconds
-def get_timestamp(job_name):
-    info = eval(urllib.urlopen("http://build.nc.cow/job/" + job_name + "/lastBuild/api/python?pretty=true").read())
-    return info["timestamp"]
-
-
 @route('/rst/<filepath:path>')
 def rst(filepath):
     return static_file(filepath, root=os.path.join(os.path.dirname(script_dir), 'raspberry', '_build', 'html'))
@@ -79,7 +54,15 @@ def page_debug():
 def page_status(job_name):
     try:
         info = server.get_job_info(job_name)
-        return '{"status": "%s", "timestamp": %d}' % (get_status(info), get_timestamp(job_name))
+
+        status = info['color']
+        if status.endswith("_anime"):
+            status = status[0:-6] + " building"
+
+        url = "http://build.nc.cow/job/" + job_name + "/lastBuild/api/python?pretty=true"
+        timestamp = eval(urllib.urlopen(url).read())["timestamp"]
+
+        return '{"status": "%s", "timestamp": %d}' % (status, timestamp)
     except:
         return None
 
@@ -88,7 +71,17 @@ def page_status(job_name):
 def page_health(job_name):
     try:
         info = server.get_job_info(job_name)
-        failed_num, total_num = get_health_report(info)
+
+        failed_num, total_num = 0, 0
+        for i in info["healthReport"]:
+            if i["description"].startswith("Test Result"):
+                m = re.match(".*?(\d[,\d]+).*?(\d[,\d]+)", i["description"])
+                if m:
+                    failed = m.group(1).replace(",", "")
+                    total = m.group(2).replace(",", "")
+                    failed_num, total_num = int(failed), int(total)
+                    break
+
         return '{"failed": %d, "total": %d}' % (failed_num, total_num)
     except:
         return None
